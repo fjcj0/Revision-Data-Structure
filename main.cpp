e112a7a42a02cfec7e1fb09e4392420dd0881a4b
omar <omarqais225@gmail.com>
2025-09-13 16:47:19 +0300

    doubly linkedlist

--- main.cpp
+++ main.cpp
@@ -1,197 +1,156 @@
 #include <iostream>
 #include <cassert>
-using namespace std;
-
 template <class T>
-class FullLinkedList {
-    struct node {
-        node* next;
+class DoublyLinkedList {
+    struct node{
+        node*next;
+        node*pre;
         T item;
     };
-
 public:
-    node* head;
-    node* tail;
+    node*head;
+    node*tail;
     int length;
-
-    FullLinkedList() {
-        head = tail = NULL;
-        length = 0;
+    DoublyLinkedList(): head{NULL},tail{NULL},length{0} {}
+    bool isEmpty(){
+        return head ==NULL;
     }
-
-    bool isEmpty() {
-        return head == NULL;
-    }
-
-    void addFirst(T newItem) {
-        node*newItemPtr = new node();
+    void InsertLast(T newItem){
+        node* newItemPtr = new node();
         newItemPtr->item = newItem;
+        newItemPtr->next = NULL;
         if(isEmpty()){
-            newItemPtr->next = NULL;
+            newItemPtr->pre = NULL;
             head = tail = newItemPtr;
-        }else{
-            newItemPtr->next = head;
-            head = newItemPtr;
-        }
-        length++;
-    }
-    
-    void addLast(T newItem){
-        node*newItemPtr = new node();
-        newItemPtr->item = newItem;
-        if(isEmpty()){
-            newItemPtr->next = NULL;
-            head = tail = newItemPtr;
-        }else{
-            newItemPtr->next = NULL;
+        } else {
+            newItemPtr->pre = tail;
             tail->next = newItemPtr;
             tail = newItemPtr;
         }
         length++;
     }
-    
-    void addAtPos(T newItem,int index){
-        if(index==0){
-            addFirst(newItem);
-        }else if (index == length){
-            addLast(newItem);
+    void InsertFirst(T newItem){
+        node* newItemPtr = new node();
+        newItemPtr->item = newItem;
+        if(isEmpty()){
+            newItemPtr->next = NULL;
+            newItemPtr->pre = NULL;
+            head = tail = newItemPtr;
         }else{
-            node*newItemPtr = new node();
-            newItemPtr->item = newItem;
-            node* cur = head;
-            node* pre = NULL;
-            for (int i = 0; i < index; i++) {
-                pre = cur;
-                cur = cur->next;
-            }
-            pre->next = newItemPtr;
-            newItemPtr->next = cur;
-            length++;
+            newItemPtr->pre = NULL;
+            newItemPtr->next = head;
+            head->pre = newItemPtr;
+            head = newItemPtr;
         }
+        length++;
     }
-
-    void removeFirst() {
-        assert(!isEmpty() && "Cannot pop from an empty linked list!");
+    void DeleteFirst(){
+        assert(!isEmpty());
         node* temp = head;
-        head = head->next;
-        if (head == NULL) {
-            tail = NULL;
+        if(head == tail) {
+            head = tail = NULL;
+        } else {
+            head = head->next;
+            head->pre = NULL;
+        }
+        temp = temp->next = NULL;
+        delete temp;
+        length--;
+    }
+    void DeleteLast(){
+        assert(!isEmpty());
+        node* temp = tail;
+        if(head == tail) {
+            head = tail = NULL;
+        } else {
+            tail = tail->pre;
+            tail->next = NULL;
         }
         delete temp;
         length--;
     }
-    
-    void removeLast() {
-        assert(!isEmpty() && "Cannot pop from an empty linked list!");
-        node * cur = head->next;
-        node * pre = head;
-        while(cur!=tail){
-            pre = cur;
-            cur =cur->next;
+    void removeAtPos(int pos){
+        assert(pos >= 0 && pos < length);
+        if(pos == 0){
+            DeleteFirst();
         }
-        pre->next = NULL;
-        tail = pre;
-        cur = cur->next = NULL;
-        delete cur;
-        length--;
-    }
-    
-    void removeAt(int index) {
-        assert(index < length && "Index is out of bounds!");
-        if (index == 0) {
-            removeFirst();
-        } else if (index == length - 1) {
-            removeLast();
-        } else {
+        else if(pos == length-1){
+            DeleteLast();
+        }
+        else{
             node* cur = head;
-            node* pre = NULL;
-            for (int i = 0; i < index; i++) {
-                pre = cur;
+            for(int i = 0; i < pos; i++){
                 cur = cur->next;
             }
-            pre->next = cur->next;
+            node* preNode = cur->pre;
+            node* nextNode = cur->next;
+            preNode->next = nextNode;
+            nextNode->pre = preNode;
             delete cur;
             length--;
         }
     }
-    
-    int findIndexOf(T element){
-        assert(!isEmpty() && "Cannot Find Index from an empty linked list!");
-        int count = 0;
-        node*cur = head;
-        while(cur!=NULL){
-            if(cur->item == element){
-                return count;
+    void insertAtPos(int pos, T newItem){
+        assert(pos >= 0 && pos <= length);
+
+        if(pos == 0){
+            InsertFirst(newItem);
+        }
+        else if(pos == length){
+            InsertLast(newItem);
+        }
+        else{
+            node* cur = head;
+            for(int i = 0; i < pos; i++){
+                cur = cur->next;
             }
-            count++;
-            cur=cur->next;
+            node* preNode = cur->pre;
+
+            node* newItemPtr = new node();
+            newItemPtr->item = newItem;
+            newItemPtr->next = cur;
+            newItemPtr->pre = preNode;
+            preNode->next = newItemPtr;
+            cur->pre = newItemPtr;
+            length++;
         }
-        return -1;
     }
-    
-    T getElementOfIndex(int index){
-        assert(!isEmpty() && "Cannot Get Element from an empty linked list!");
-        assert(index >= 0 && index < length && "Index is out of bounds!");
-        if(index==0) return getHead();
-        else if (index==length-1)return getTail();
-        node* cur = head;
-        for (int i = 0; i < index; i++) {
-            cur = cur->next;
-        }
-        return cur->item;
+    T getTail(){
+        assert(!isEmpty());
+        return tail->item;
     }
-    
+    T getHead(){
+        assert(!isEmpty());
+        return head->item;
+    }
     void print(){
         node*cur = head;
-        std::cout << "[";
+        std::cout <<  std::endl << "[";
         while(cur!=NULL){
             std::cout << cur->item;
             if(cur->next != NULL)std::cout << ",";
             cur=cur->next;
         }
-        std::cout << "]"<<std::endl;
-    }
-    
-    T getHead() {
-        assert(!isEmpty() && "Linked list is empty, no head!");
-        return head->item;
-    }
-    
-    T getTail(){
-        assert(!isEmpty() && "Linked list is empty, no tail!");
-        return tail->item;
-    }
-    
-    int size() {
-        return length;
-    }
-    
-    void reverse(){
-        assert(!isEmpty() && "Linked list is empty, no reverse!");
-        node*cur = head;
-        node*nxt = head->next;
-        node*pre = NULL;
-        while(cur!=NULL){
-            nxt = cur->next;
-            cur->next = pre;
-            pre=cur;
-            cur=nxt;
-        }
-        head = pre;
+        std::cout << "]" << std::endl;
     }
 };
-
 int main() {
-    FullLinkedList<int> list;
-    list.addFirst(10);
-    list.addFirst(20);
-    list.addFirst(30);
-    list.addAtPos(100, 2);
-    list.addAtPos(0, 0);
-    list.print();
-    std::cout << list.findIndexOf(100)<<std::endl;
-    std::cout << list.getElementOfIndex(2) << std::endl;
-    list.reverse();
-    list.print();
+    DoublyLinkedList<int> dll;
+    dll.InsertLast(10);
+    dll.InsertLast(20);
+    dll.InsertLast(30);
+    dll.print();
+    dll.InsertFirst(5);
+    dll.print();
+    dll.insertAtPos(2, 15);
+    dll.print();
+    dll.DeleteFirst();
+    dll.print();
+    dll.DeleteLast();
+    dll.print();
+    dll.removeAtPos(1);
+    dll.print();
+    std::cout << std::endl<<  "Head: " << dll.getHead() << std::endl << std::endl;
+    std::cout << "Tail: " << dll.getTail() << std::endl;
     return 0;
 }
