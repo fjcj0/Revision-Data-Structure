1b90f665d9c22a07a60d8ae1ed924eeae9bbbcc4
omar <omarqais225@gmail.com>
2025-09-13 23:13:33 +0300

    Merge Sort

--- main.cpp
+++ main.cpp
@@ -1,35 +1,71 @@
 #include<iostream>
 using namespace std;
-void insertionSort(int arr[], int n)
+void merge(int arr[], int l, int m, int r)
 {
-    int key, j;
-    for (int i = 1; i < n; i++)
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int *L=new int[n1], *R=new int[n2];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    i = j = 0;
+    k = l;
+    while (i < n1 && j < n2)
     {
-        key = arr[i];
-        j = i - 1;
-
-        while (j >= 0 && arr[j] < key)
+        if (L[i] <= R[j])
         {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+            arr[k] = L[i];
+            i++;
         }
-        arr[j + 1] = key;
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
     }
 }
-void printArray(int arr[], int n)
+void mergeSort(int arr[], int l, int r)
 {
-    cout << "Intersection Sort: [";
-    for (int i = 0; i < n; i++){
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+void print(int arr[], int n)
+{
+    cout << "Merge Sort: [";
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i];
-        if(i<n-1)cout << ",";
+        if(i<n-1){
+            cout << ",";
+        }
     }
     cout << "]" << endl;
 }
 int main()
 {
-        int arr[] = { 80 , 90 ,60 ,30 ,50 ,70 ,40 };
-        int n = sizeof(arr) / sizeof(arr[0]);//28/4=7
-        insertionSort(arr, n);
-        printArray(arr, n);
-        return 0;
+    int arr[] = { 60,10,20,5,60,70 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    mergeSort(arr, 0, n-1);
+    print(arr,n);
+    return 0;
 }
